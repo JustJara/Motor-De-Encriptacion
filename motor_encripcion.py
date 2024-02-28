99c8066d57391855886513e6a96cd8fc5980f732
Felipe <villajaramillofelipe4@gmail.com>
2024-02-28 13:33:22 -0500

    Feature: 14 casos de prueba

---
+++ motor_encripcion.py
@@ -0,0 +1,18 @@
+class EmptyEncryptMessage(Exception):
+    pass
+
+class InvalidSecretKey(Exception):
+    pass
+
+class MessageIsNotEncrypted(Exception):
+    pass
+
+class EmptySecretKey(Exception):
+    pass
+
+def encriptar():
+    pass
+
+def desencriptar():
+    pass
+
