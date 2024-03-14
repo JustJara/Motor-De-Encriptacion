f23184d7efbbd64e0ccd47a2271b2d2619c2f698
Felipe <villajaramillofelipe4@gmail.com>
2024-03-14 15:25:14 -0500

    Feature: Updated variable names, methods name & source folder

---
+++ src/encriptation_algorithm.py
@@ -0,0 +1,21 @@
+class EmptyEncryptMessage(Exception):
+    pass
+
+class InvalidSecretKey(Exception):
+    pass
+
+class MessageIsNotEncrypted(Exception):
+    pass
+
+class EmptySecretKey(Exception):
+    pass
+
+class EmptyInputValuesError(Exception):
+    pass
+
+def encrypt():
+    pass
+
+def decrypt():
+    pass
+
